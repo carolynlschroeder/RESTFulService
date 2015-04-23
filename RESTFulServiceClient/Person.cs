3bd74020a7f77f29f069af0fc9dd2c7925e60b04
Carolyn Schroeder <carolynlschroeder@hotmail.com>
2015-04-22 21:30:38 -0700

    Retrieved Response from GetAllPersons

--- RESTFulServiceClient/Person.cs
+++ RESTFulServiceClient/Person.cs
@@ -1,26 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Serialization;
 
-namespace RESTFulService
+namespace RESTFulServiceClient
 {
-    [DataContract]
+    [XmlType("Person")]
     public class Person
     {
-        [DataMember]
         [XmlElement("PersonId")]
         public int PersonId { get; set; }
-        [DataMember]
         [XmlElement("PersonFirst")]
         public string PersonFirst { get; set; }
-        [DataMember]
         [XmlElement("PersonLast")]
         public string PersonLast { get; set; }
-        [DataMember]
         [XmlElement("Income")]
         public double Income { get; set; }
     }
+
 }
--- RESTFulServiceClient/Program.cs
+++ RESTFulServiceClient/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace RESTFulServiceClient
 {
@@ -16,13 +18,21 @@ namespace RESTFulServiceClient
 
             req.Method = "GET";
 
-            HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
+           HttpWebResponse resp = req.GetResponse() as HttpWebResponse;
             if (resp.StatusCode == HttpStatusCode.OK)
-            {
+           {
                 using (Stream respStream = resp.GetResponseStream())
                 {
-                    StreamReader reader = new StreamReader(respStream, Encoding.UTF8);
-                    Console.WriteLine(reader.ReadToEnd());
+
+                    var strReader = new StreamReader(respStream, Encoding.UTF8);
+                    var reader = new StringReader(RemoveAllNamespaces(strReader.ReadToEnd()));
+                     System.Xml.Serialization.XmlSerializer xmlSer = new System.Xml.Serialization.XmlSerializer(typeof(List<Person>));
+                        var personList = (List<Person>)xmlSer.Deserialize(reader);
+
+                    foreach (var p in personList)
+                    {
+                        Console.WriteLine(String.Format("{0} {1}", p.PersonFirst, p.PersonLast));
+                    }
                 }
             }
             else
@@ -31,5 +41,29 @@ namespace RESTFulServiceClient
             }
             Console.Read();
         }
+
+        public static string RemoveAllNamespaces(string xmlDocument)
+        {
+            XElement xmlDocumentWithoutNs = RemoveAllNamespaces(XElement.Parse(xmlDocument));
+
+            return xmlDocumentWithoutNs.ToString();
+        }
+
+        //Core recursion function
+        private static XElement RemoveAllNamespaces(XElement xmlDocument)
+        {
+            if (!xmlDocument.HasElements)
+            {
+                XElement xElement = new XElement(xmlDocument.Name.LocalName);
+                xElement.Value = xmlDocument.Value;
+
+                foreach (XAttribute attribute in xmlDocument.Attributes())
+                    xElement.Add(attribute);
+
+                return xElement;
+            }
+            return new XElement(xmlDocument.Name.LocalName, xmlDocument.Elements().Select(el => RemoveAllNamespaces(el)));
+        }
+
     }
 }
